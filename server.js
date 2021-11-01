e484a1c61f3c8c18cf343310ee6d4a040daeeddb
John Moore <johnkmoore98@gmail.com>
2021-11-01 15:38:05 -0400

    /app/user endpoint

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/new/user", (req, res) => {
+app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
 	res.json({"message":`${info.changes} record created: ID ${info.lastInsertRowid}`});
@@ -41,6 +41,11 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/", (req, resp) => {
+	const stmt = db.prepare("SELECT (*) FROM userinfo WHERE id = ?")
+	const res = stmt.get(id);
+	res.status(200).json(res);
+})
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
